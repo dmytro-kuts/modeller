41d139d772883c1af48ff33c6afa23bf82c6d736
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-04-03 04:35:55 +0300

    add Blog single artcile and faq. The structure of the header has changed, the JS code has been redesigned for menuDropDown and form. Small fixes

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,41 +5,88 @@ import { flsModules } from './modules.js';
 
 /* ==============================================================================================
 =============================  Menu  ============================================================*/
-
-window.onload = function () {
+function menuDropDown() {
   document.addEventListener('click', documentActions);
 
   function documentActions(e) {
     const targetElement = e.target;
-    if (targetElement.closest('.menu__link') || targetElement.closest('.menu__link span')) {
-      const menuItem = targetElement.closest('.menu__item');
+    if (
+      (bodyLockStatus && targetElement.closest('.menu__link, .actions-header__button ')) ||
+      targetElement.closest('.menu__link span, .actions-header__button span')
+    ) {
+      const menuItem = targetElement.closest('.menu__item, .actions-header__item');
       if (menuItem.classList.contains('_hover')) {
         menuItem.classList.remove('_hover');
+        bodyUnlock();
         document.documentElement.classList.remove('menu-open');
       } else {
         const openMenuItem = document.querySelector('.menu__item._hover');
         if (openMenuItem) {
           openMenuItem.classList.remove('_hover');
         }
+        bodyLock();
         menuItem.classList.add('_hover');
         document.documentElement.classList.add('menu-open');
       }
     }
     if (
-      !targetElement.closest('.menu__item') &&
-      document.querySelectorAll('.menu__item._hover').length > 0
+      !targetElement.closest('.menu__item, .actions-header__item') &&
+      document.querySelectorAll('.menu__item._hover, .actions-header__item._hover').length > 0
     ) {
-      const openMenuItem = document.querySelector('.menu__item._hover');
+      const openMenuItem = document.querySelector(
+        '.menu__item._hover, .actions-header__item._hover',
+      );
       openMenuItem.classList.remove('_hover');
+      bodyUnlock();
       document.documentElement.classList.remove('menu-open');
     }
   }
-};
+
+  let bodyLockStatus = true;
+
+  let bodyLock = (delay = 500) => {
+    let body = document.querySelector('body');
+    if (bodyLockStatus) {
+      let lock_padding = document.querySelectorAll('[data-lp]');
+      for (let index = 0; index < lock_padding.length; index++) {
+        const el = lock_padding[index];
+        el.style.paddingRight =
+          window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+      }
+      body.style.paddingRight =
+        window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+      document.documentElement.classList.add('lock');
+
+      bodyLockStatus = false;
+      setTimeout(function () {
+        bodyLockStatus = true;
+      }, delay);
+    }
+  };
+  let bodyUnlock = (delay = 500) => {
+    let body = document.querySelector('body');
+    if (bodyLockStatus) {
+      let lock_padding = document.querySelectorAll('[data-lp]');
+      setTimeout(() => {
+        for (let index = 0; index < lock_padding.length; index++) {
+          const el = lock_padding[index];
+          el.style.paddingRight = '0px';
+        }
+        body.style.paddingRight = '0px';
+        document.documentElement.classList.remove('lock');
+      }, delay);
+      bodyLockStatus = false;
+      setTimeout(function () {
+        bodyLockStatus = true;
+      }, delay);
+    }
+  };
+}
 /* =========================== Menu ===================================================================
 =======================================================================================================*/
 /* ==============================================================================================
 =============================  Form  ============================================================*/
-window.addEventListener('click', () => {
+function form() {
   const form = document.querySelector('[data-form]');
   const nameInput = document.querySelector('#input_name');
   const emailInput = document.querySelector('#input_email');
@@ -47,15 +94,13 @@ window.addEventListener('click', () => {
   const messageInput = document.querySelector('#input_question');
   const submitButton = document.querySelector('.form__button');
 
-
-
   const validateName = () => {
     const nameValue = nameInput.value.trim();
     const nameRegex = /^[a-zA-Z]{2,}$/;
-    if (nameValue === '' || !nameRegex.test(nameValue)) {
+    if (nameInput && (nameValue === '' || !nameRegex.test(nameValue))) {
       nameInput.classList.add('_form-error');
       nameInput.previousElementSibling.textContent = nameInput.dataset.error;
-    } else {
+    } else if (nameInput) {
       nameInput.classList.remove('_form-error');
       nameInput.previousElementSibling.textContent = '';
     }
@@ -65,10 +110,10 @@ window.addEventListener('click', () => {
   const validateEmail = () => {
     const emailValue = emailInput.value.trim();
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailValue === '' || !emailRegex.test(emailValue)) {
+    if (emailInput && (emailValue === '' || !emailRegex.test(emailValue))) {
       emailInput.classList.add('_form-error');
       emailInput.previousElementSibling.textContent = emailInput.dataset.error;
-    } else {
+    } else if (emailInput) {
       emailInput.classList.remove('_form-error');
       emailInput.previousElementSibling.textContent = '';
     }
@@ -78,10 +123,10 @@ window.addEventListener('click', () => {
   const validatePhone = () => {
     const phoneValue = phoneInput.value.trim();
     const phoneRegex = /^(\+?\d+)?(\s|\()?\d{3}(\s|\))?(\s|\-)?\d{3}(\s|\-)?\d{2}(\s|\-)?\d{2}$/;
-    if (phoneValue !== '' && !phoneRegex.test(phoneValue)) {
+    if (phoneInput && phoneValue !== '' && !phoneRegex.test(phoneValue)) {
       phoneInput.classList.add('_form-error');
       phoneInput.previousElementSibling.textContent = phoneInput.dataset.error;
-    } else {
+    } else if (phoneInput) {
       phoneInput.classList.remove('_form-error');
       phoneInput.previousElementSibling.textContent = '';
     }
@@ -90,10 +135,10 @@ window.addEventListener('click', () => {
 
   const validateMessage = () => {
     const messageValue = messageInput.value.trim();
-    if (messageValue.length < 5 || messageValue.length > 200) {
+    if (messageInput && (messageValue.length < 5 || messageValue.length > 200)) {
       messageInput.classList.add('_form-error');
       messageInput.previousElementSibling.textContent = messageInput.dataset.error;
-    } else {
+    } else if (messageInput) {
       messageInput.previousElementSibling.textContent = '';
       messageInput.classList.remove('_form-error');
     }
@@ -104,11 +149,11 @@ window.addEventListener('click', () => {
     const requiredInputs = form.querySelectorAll('[data-required]');
     let isAllValid = true;
     requiredInputs.forEach((input) => {
-      if (input.value.trim() === '') {
+      if (input && input.value.trim() === '') {
         input.classList.add('_form-error');
         input.previousElementSibling.textContent = input.dataset.error;
         isAllValid = false;
-      } else if (input.classList.contains('_form-error')) {
+      } else if (input && input.classList.contains('_form-error')) {
         isAllValid = false;
       }
     });
@@ -118,12 +163,18 @@ window.addEventListener('click', () => {
       submitButton.classList.remove('_active');
     }
   };
-
-  nameInput.addEventListener('input', validateName);
-  emailInput.addEventListener('input', validateEmail);
-  phoneInput.addEventListener('input', validatePhone);
-  messageInput.addEventListener('input', validateMessage);
-
+  if (nameInput) {
+    nameInput.addEventListener('input', validateName);
+  }
+  if (emailInput) {
+    emailInput.addEventListener('input', validateEmail);
+  }
+  if (phoneInput) {
+    phoneInput.addEventListener('input', validatePhone);
+  }
+  if (messageInput) {
+    messageInput.addEventListener('input', validateMessage);
+  }
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     if (!submitButton.classList.contains('_active')) {
@@ -148,73 +199,13 @@ window.addEventListener('click', () => {
         form.classList.add('_error');
       });
   });
-});
-//====================================================================
-// window.addEventListener('load', () => {
-//   const form = document.querySelector('[data-form]');
-//   const submitButton = form.querySelector('.form__button');
+}
 
-//   const validateInput = (input) => {
-//     const value = input.value.trim();
-//     const regex = input.dataset.regex ? new RegExp(input.dataset.regex) : null;
-//     if (value === '' || (regex && !regex.test(value))) {
-//       input.classList.add('_form-error');
-//       input.previousElementSibling.textContent = input.dataset.error;
-//     } else {
-//       input.classList.remove('_form-error');
-//       input.previousElementSibling.textContent = '';
-//     }
-//   };
-
-//   const validateForm = () => {
-//     const requiredInputs = form.querySelectorAll('[data-required]');
-//     let isAllValid = true;
-//     requiredInputs.forEach((input) => {
-//       if (input.value.trim() === '') {
-//         input.classList.add('_form-error');
-//         input.previousElementSibling.textContent = input.dataset.error;
-//         isAllValid = false;
-//       } else if (input.classList.contains('_form-error')) {
-//         isAllValid = false;
-//       }
-//     });
-//     if (isAllValid) {
-//       submitButton.classList.add('_active');
-//     } else {
-//       submitButton.classList.remove('_active');
-//     }
-//   };
-
-//   form.addEventListener('input', (event) => {
-//     if (event.target.matches('[data-required]')) {
-//       validateInput(event.target);
-//       validateForm();
-//     }
-//   });
-
-//   form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     if (submitButton.classList.contains('_active')) {
-//       form.classList.add('_sending');
-//       const formData = new FormData(form);
-//       fetch(form.action, {
-//         method: form.method,
-//         body: formData,
-//       })
-//         .then((response) => {
-//           if (response.ok) {
-//             form.classList.remove('_sending');
-//             form.classList.add('_success');
-//             form.reset();
-//           } else {
-//             throw new Error(response.statusText);
-//           }
-//         })
-//         .catch(() => {
-//           form.classList.add('_error');
-//         });
-//     }
-//   });
-// });
 /* ==========================  Form  =====================================================
+=========================================================================================*/
+window.addEventListener('load', function (e) {
+  menuDropDown();
+  form();
+});
+/* ===============================================================================
 =========================================================================================*/
